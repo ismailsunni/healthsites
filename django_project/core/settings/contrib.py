32415ee407a79c939749b7d54adb3809114b6377
Ismail Sunni <imajimatika@gmail.com>
2015-10-12 20:08:41 +0700

    Add template context processors.

--- django_project/core/settings/contrib.py
+++ django_project/core/settings/contrib.py
@@ -33,6 +33,18 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'social.apps.django_app.context_processors.backends',
+    'social.apps.django_app.context_processors.login_redirect',
+)
+
 BROKER_URL = 'amqp://guest:guest@%s:5672//' % os.environ['RABBITMQ_HOST']
 
 CELERY_ACCEPT_CONTENT = ['json']
